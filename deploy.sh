7812402c61dba10f957459383d4c3143646bd6dd
sukeshdonepudi <sukeshdonepudi@gmail.com>
2023-03-26 15:33:39 -0400

    second commit

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ docker push sukeshdonepudi/flask
 
 # Docker build for backend React Frontend
 
-Change Directory to “react”-app
+# Change Directory to “react”-app
 
 cd Frontend-react
 docker build -t sukeshdonepudi/react -f Dockerfile .
@@ -21,7 +21,7 @@ docker push sukeshdonepudi/react
 
 # Deploy Kubernetes Yaml Files in the docker Kubernetes cluster
 
-Change Directory to the Kubernetes
+# Change Directory to the Kubernetes
 
 Kubectl apply -f Deployment-Flask.yml
 Kubectl apply -f Deployment-React.yml
